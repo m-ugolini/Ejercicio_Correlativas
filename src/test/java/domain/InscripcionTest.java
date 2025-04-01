3ad077254bbe15d24465fee07ecfa8c9b65dcac2
m-ugolini <mugolini@frba.utn.edu.ar>
2025-04-01 08:39:19 -0300

    Cambios

--- src/test/java/domain/InscripcionTest.java
+++ src/test/java/domain/InscripcionTest.java
@@ -4,10 +4,6 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.util.HashSet;
-import java.util.Set;
-import java.util.stream.Stream;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 class InscripcionTest {
